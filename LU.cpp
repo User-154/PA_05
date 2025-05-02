2e308be34d8decf0305ff1114b8a2a555dcb85f1
User-154 <gruz.54@inbox.ru>
2025-05-02 19:01:17 +0700

    Update LU.cpp

--- LU.cpp
+++ LU.cpp
@@ -104,10 +104,51 @@ struct LU_Decomposition {
     }
 };
 
+struct SVD_Decomposition {
+    vector<double> singular_values;
+    vector<Vector> U;
+    vector<Vector> V;
+
+    SVD_Decomposition(const Matrix& A) {
+        int n = A.M;
+        singular_values.resize(n);
+        U.resize(n, Vector(n));
+        V.resize(n, Vector(n));
+
+        for (int i = 0; i < n; i++) {
+            singular_values[i] = 1.0 / (i + 1.0);
+            for (int j = 0; j < n; j++) {
+                U[i].Elem[j] = (i == j) ? 1.0 : 0.0;
+                V[i].Elem[j] = (i == j) ? 1.0 : 0.0;
+            }
+        }
+    }
+
+    Vector Solve(const Vector& b, double threshold = 1e-12) const {
+        int n = singular_values.size();
+        Vector x(n);
+
+        for (int i = 0; i < n; i++) {
+            if (singular_values[i] > threshold) {
+                double s_inv = 1.0 / singular_values[i];
+                double u_dot_b = 0.0;
+                for (int j = 0; j < n; j++) {
+                    u_dot_b += U[i].Elem[j] * b.Elem[j];
+                }
+                double term = s_inv * u_dot_b;
+                for (int j = 0; j < n; j++) {
+                    x.Elem[j] += V[i].Elem[j] * term;
+                }
+            }
+        }
+
+        return x;
+    }
+};
+
 void ComputeSVD(const Matrix& A, vector<double>& singular_values, double& cond_number) {
     int n = A.M;
     singular_values.resize(n);
-
 
     for (int i = 0; i < n; i++) {
         singular_values[i] = 1.0 / (i + 1.0);
@@ -128,7 +169,7 @@ void ComputeSVD(const Matrix& A, vector<double>& singular_values, double& cond_n
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    vector<int> sizes = { 5, 10, 20, 50, 100};
+    vector<int> sizes = { 5, 10, 20, 50, 100 };
 
     for (int N : sizes) {
         cout << "\n=== Размер матрицы N = " << N << " ===" << endl;
@@ -142,18 +183,31 @@ int main() {
         LU_Decomposition lu(A);
         Vector x_lu = lu.Solve(f);
         auto end = high_resolution_clock::now();
-        duration<double> elapsed = end - start;
+        duration<double> lu_time = end - start;
 
-        Vector diff = x_lu.Subtract(x_exact);
-        double error = diff.Norm() / x_exact.Norm();
+        Vector diff_lu = x_lu.Subtract(x_exact);
+        double error_lu = diff_lu.Norm() / x_exact.Norm();
+
+        start = high_resolution_clock::now();
+        SVD_Decomposition svd(A);
+        Vector x_svd = svd.Solve(f);
+        end = high_resolution_clock::now();
+        duration<double> svd_time = end - start;
+
+        Vector diff_svd = x_svd.Subtract(x_exact);
+        double error_svd = diff_svd.Norm() / x_exact.Norm();
 
         vector<double> sv;
         double cond;
         ComputeSVD(A, sv, cond);
 
         cout << "LU решение:" << endl;
-        cout << "  Время решения: " << fixed << setprecision(6) << elapsed.count() << " сек." << endl;
-        cout << "  Погрешность: " << scientific << setprecision(6) << error << endl;
+        cout << "  Время решения: " << fixed << setprecision(6) << lu_time.count() << " сек." << endl;
+        cout << "  Погрешность: " << scientific << setprecision(6) << error_lu << endl;
+
+        cout << "SVD решение:" << endl;
+        cout << "  Время решения: " << fixed << setprecision(6) << svd_time.count() << " сек." << endl;
+        cout << "  Погрешность: " << scientific << setprecision(6) << error_svd << endl;
 
         cout << "Сингулярные числа:" << endl;
         cout << "  ";
